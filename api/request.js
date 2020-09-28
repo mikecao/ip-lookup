43a24f7208d28be2ab6bc19dfa382c49a4e1b118
Mike Cao <mike@mikecao.com>
2020-09-28 09:34:52 -0700

    Read files.

--- api/request.js
+++ api/request.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 export default (req, res) => {
-  res.status(200).json({ headers: req.headers, dirname: __dirname });
-}
+  const asn = fs.readFileSync(path.join(__dirname, '../public', 'GeoLite2-ASN.mmdb'), 'utf8');
+  const city = fs.readFileSync(path.join(__dirname, '../public', 'GeoLite2-City.mmdb'), 'utf8');
+  const country = fs.readFileSync(path.join(__dirname, '../public', 'GeoLite2-Country.mmdb'), 'utf8');
+
+  res
+    .status(200)
+    .json({
+      headers: req.headers,
+      dirname: __dirname,
+      files: { asn: asn.length, city: city.length, country: country.length },
+    });
+};
