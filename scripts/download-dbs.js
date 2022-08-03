35aba49bda9c268edb648ba9823581611e60c76a
Mike Cao <mike@mikecao.com>
2022-08-02 20:48:14 -0700

    Renamed to geo.

--- scripts/download-dbs.js
+++ scripts/download-dbs.js
@@ -10,7 +10,7 @@ let urls = [
   'https://raw.githubusercontent.com/GitSquared/node-geolite2-redist/master/redist/GeoLite2-Country.tar.gz',
 ];
 
-const dest = path.resolve(__dirname, '../public/.dbs/');
+const dest = path.resolve(__dirname, '../public/geo/');
 
 if (!fs.existsSync(dest)) {
   fs.mkdirSync(dest);
